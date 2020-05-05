fd1774565b86c3f7f236f55a09272a70beb422b8
yekaitao <53636166+yekaitao@users.noreply.github.com>
2020-05-05 22:23:16 +0800

    Create LoginActivity.java

---
+++ app/src/main/java/com/ysq/wifisignin/ui/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ysq.wifisignin.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ysq.wifisignin.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
